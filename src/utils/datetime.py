0aff3e24be2d56c4eba702c33ed9debdce12a3b4
Joseph <51335923+Otterboi@users.noreply.github.com>
2026-03-20 17:20:16 -0400

    Create datetime.py

---
+++ src/utils/datetime.py
@@ -0,0 +1,21 @@
+"""
+Datetime utility functions
+"""
+
+from datetime import datetime, timezone
+
+
+def utcnow() -> datetime:
+    """
+    Get current UTC time (timezone-aware).
+
+    Replacement for deprecated datetime.utcnow()
+
+    Returns:
+        Current UTC datetime with timezone info
+    """
+    return datetime.now(timezone.utc)
+
+# scribe: a function that adds 2 numbers
+def add_numbers(a, b):
+    return a + b
